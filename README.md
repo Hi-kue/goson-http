684ce04e5fd38b1a9315fb501ab50672f777cd73
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-05-30 14:00:54 -0400

    Intial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Goson HTTP Automotive API
---
+++ main.go
@@ -0,0 +1,20 @@
+package goson_http
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Building REST APIs with Go 1.22")
+
+	mux := http.NewServeMux() // New HTTP Multiplexer
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
+		fmt.Println(err.Error())
+	}
+}
